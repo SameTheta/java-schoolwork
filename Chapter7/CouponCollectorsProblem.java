ed9ac8a533ed22757205e64c5239a1ffbd653bfb
SameTheta <sametheta@tutanota.com>
2024-03-15 12:21:59 -0400

    Coupon Problem Finished

--- Chapter7/CouponCollectorsProblem.java
+++ Chapter7/CouponCollectorsProblem.java
@@ -1,5 +1,26 @@
 public class CouponCollectorsProblem {
     public static void main(String[] args) {
         //huh@H!??!
+        final int NUMBER_OF_CARDS = 52;
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                            "Jack", "Queen", "King"};
+        boolean[] found = new boolean[4];
+
+        int numberOfPicks = 0;
+
+        for (int count = 0; count < 4; ++numberOfPicks) {
+            int index = (int) (Math.random() * NUMBER_OF_CARDS);
+            if (!found[index / 13]) {
+                found[index / 13] = true;
+                count++;
+
+                String suit = suits[index / 13];
+                String rank = ranks[index % 13];
+                System.out.println(rank + " of " + suit);
+            }
+        }
+        
+        System.out.println("Number of picks: " + numberOfPicks);
     }
 }
